70ad2e21e496fc11ec8c0f1d03d869ca9c692378
TanaseMariusatstarion <tm.manafu@stariongroup.eu>
2026-08-03 11:26:33 +0200

    SDK tests

---
+++ Mycelium.SDK.Tests/Dto/DtoContractTestFixture.cs
@@ -0,0 +1,347 @@
+// ------------------------------------------------------------------------------------------------
+//  <copyright file="DtoContractTestFixture.cs" company="Starion Group S.A.">
+// 
+//    Copyright 2026 Starion Group S.A.
+//    SPDX-License-Identifier: Apache-2.0
+// 
+//  </copyright>
+//  ------------------------------------------------------------------------------------------------
+
+namespace Mycelium.SDK.Tests.Dto
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using Mycelium.SDK.DTO;
+
+    /// <summary>
+    /// Verifies the public runtime contract of the generated FunctionalData DTOs.
+    /// </summary>
+    [TestFixture]
+    public class DtoContractTestFixture
+    {
+        private const string DtoNamespace = "Mycelium.SDK.DTO";
+
+        private static readonly Type[] DtoInterfaceTypes =
+        [
+            typeof(IThing),
+            typeof(IAuditableThing),
+            typeof(IBranchProtectionRule),
+            typeof(IComment),
+            typeof(IFunctionalProject),
+            typeof(IFunctionalProjectPolicy),
+            typeof(IOrganization),
+            typeof(IOrganizationMember),
+            typeof(IOrganizationPolicy),
+            typeof(IOwnership),
+            typeof(IProjectMember),
+            typeof(IReview),
+            typeof(IUser)
+        ];
+
+        private static readonly Type[] ConcreteDtoTypes =
+        [
+            typeof(BranchProtectionRule),
+            typeof(Comment),
+            typeof(FunctionalProject),
+            typeof(FunctionalProjectPolicy),
+            typeof(Organization),
+            typeof(OrganizationMember),
+            typeof(OrganizationPolicy),
+            typeof(Ownership),
+            typeof(ProjectMember),
+            typeof(Review),
+            typeof(User)
+        ];
+
+        private static readonly string[] InitializedCollectionProperties =
+        [
+            "BranchProtectionRule.DefaultReviewers",
+            "BranchProtectionRule.MergeAllowedFor",
+            "Comment.Replies",
+            "FunctionalProject.BranchRules",
+            "FunctionalProject.Defines",
+            "FunctionalProject.Involves",
+            "FunctionalProject.Reviews",
+            "Organization.InvolvedUser",
+            "Organization.Projects",
+            "ProjectMember.Owns",
+            "Review.Comments",
+            "Review.Reviewers",
+            "User.IsPartOfOrganizations",
+            "User.IsPartOfProjects"
+        ];
+
+        [Test]
+        public void Verify_that_DTO_interface_coverage_matches_the_reviewed_contract()
+        {
+            var expectedInterfaceTypes = DtoInterfaceTypes
+                .OrderBy(type => type.Name, StringComparer.Ordinal)
+                .ToArray();
+
+            var actualInterfaceTypes = typeof(IThing).Assembly
+                .GetExportedTypes()
+                .Where(type => type.Namespace == DtoNamespace && type.IsInterface)
+                .OrderBy(type => type.Name, StringComparer.Ordinal)
+                .ToArray();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(expectedInterfaceTypes, Has.Length.EqualTo(13));
+                Assert.That(
+                    actualInterfaceTypes,
+                    Is.EqualTo(expectedInterfaceTypes),
+                    "The public DTO interface set contains missing or extra interfaces.");
+            });
+        }
+
+        [TestCaseSource(nameof(RepresentativeInterfacePropertyContracts))]
+        public void Verify_that_representative_interface_property_shape_matches_the_contract(
+            Type interfaceType,
+            string propertyName,
+            Type expectedPropertyType)
+        {
+            var property = interfaceType.GetProperty(
+                propertyName,
+                BindingFlags.Public
+                | BindingFlags.Instance
+                | BindingFlags.DeclaredOnly);
+
+            Assert.That(
+                property,
+                Is.Not.Null,
+                $"Interface '{interfaceType.Name}' does not declare property '{propertyName}'.");
+
+            Assert.That(
+                property!.PropertyType,
+                Is.EqualTo(expectedPropertyType),
+                $"Property '{interfaceType.Name}.{propertyName}' has an unexpected type.");
+        }
+
+        [Test]
+        public void Verify_that_concrete_DTO_coverage_matches_the_reviewed_contract()
+        {
+            var expectedConcreteTypes = ConcreteDtoTypes
+                .OrderBy(type => type.Name, StringComparer.Ordinal)
+                .ToArray();
+
+            var actualConcreteTypes = typeof(IThing).Assembly
+                .GetExportedTypes()
+                .Where(type => type.Namespace == DtoNamespace && type.IsClass && !type.IsAbstract)
+                .OrderBy(type => type.Name, StringComparer.Ordinal)
+                .ToArray();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(expectedConcreteTypes, Has.Length.EqualTo(11));
+
+                Assert.That(
+                    actualConcreteTypes,
+                    Is.EqualTo(expectedConcreteTypes),
+                    "The public concrete DTO set contains missing or extra classes.");
+
+                foreach (var concreteType in ConcreteDtoTypes)
+                {
+                    Assert.That(
+                        concreteType.GetConstructor(Type.EmptyTypes),
+                        Is.Not.Null,
+                        $"'{concreteType.Name}' must provide a public parameterless constructor.");
+                }
+            });
+        }
+
+        [Test]
+        public void Verify_that_only_derived_properties_are_getter_only()
+        {
+            var interfaceProperties = DtoInterfaceTypes
+                .SelectMany(interfaceType =>
+                    interfaceType
+                        .GetProperties(
+                            BindingFlags.Public
+                            | BindingFlags.Instance
+                            | BindingFlags.DeclaredOnly)
+                        .Select(property => (DeclaringType: interfaceType, Property: property)));
+
+            var concreteProperties = ConcreteDtoTypes
+                .SelectMany(concreteType =>
+                    concreteType
+                        .GetProperties(
+                            BindingFlags.Public
+                            | BindingFlags.Instance
+                            | BindingFlags.DeclaredOnly)
+                        .Select(property => (DeclaringType: concreteType, Property: property)));
+
+            Assert.Multiple(() =>
+            {
+                foreach (var contractProperty in interfaceProperties.Concat(concreteProperties))
+                {
+                    var displayName = $"{contractProperty.DeclaringType.Name}.{contractProperty.Property.Name}";
+
+                    Assert.That(
+                        contractProperty.Property.GetGetMethod(nonPublic: true)?.IsPublic,
+                        Is.True,
+                        $"'{displayName}' must have a public getter.");
+
+                    var setter = contractProperty.Property.GetSetMethod(nonPublic: true);
+
+                    if (IsDerivedProperty(contractProperty.DeclaringType, contractProperty.Property))
+                    {
+                        Assert.That(
+                            setter,
+                            Is.Null,
+                            $"Derived property '{displayName}' must be getter-only.");
+                    }
+                    else
+                    {
+                        Assert.That(
+                            setter?.IsPublic,
+                            Is.True,
+                            $"Writable property '{displayName}' must have a public setter.");
+                    }
+                }
+            });
+        }
+
+        [Test]
+        public void Verify_that_collection_properties_are_initialized_and_empty()
+        {
+            var collectionProperties = ConcreteDtoTypes
+                .SelectMany(concreteType =>
+                    concreteType
+                        .GetProperties(
+                            BindingFlags.Public
+                            | BindingFlags.Instance
+                            | BindingFlags.DeclaredOnly)
+                        .Where(property => IsList(property.PropertyType))
+                        .Select(property => (DeclaringType: concreteType, Property: property)))
+                .OrderBy(
+                    item => $"{item.DeclaringType.Name}.{item.Property.Name}",
+                    StringComparer.Ordinal)
+                .ToArray();
+
+            var actualCollectionPropertyNames = collectionProperties
+                .Select(item => $"{item.DeclaringType.Name}.{item.Property.Name}")
+                .ToArray();
+
+            Assert.That(
+                actualCollectionPropertyNames,
+                Is.EqualTo(InitializedCollectionProperties),
+                "The DTO collection-property set is incomplete or contains unexpected properties.");
+
+            Assert.Multiple(() =>
+            {
+                foreach (var collectionProperty in collectionProperties)
+                {
+                    var displayName =
+                        $"{collectionProperty.DeclaringType.Name}.{collectionProperty.Property.Name}";
+
+                    var instance = Activator.CreateInstance(collectionProperty.DeclaringType);
+
+                    Assert.That(
+                        instance,
+                        Is.Not.Null,
+                        $"'{collectionProperty.DeclaringType.Name}' could not be constructed.");
+
+                    if (instance is null)
+                    {
+                        continue;
+                    }
+
+                    var value = collectionProperty.Property.GetValue(instance);
+
+                    Assert.That(
+                        value,
+                        Is.Not.Null,
+                        $"Collection property '{displayName}' was not initialized.");
+
+                    Assert.That(
+                        value,
+                        Is.InstanceOf<System.Collections.ICollection>(),
+                        $"Collection property '{displayName}' does not expose a supported collection.");
+
+                    if (value is System.Collections.ICollection collection)
+                    {
+                        Assert.That(
+                            collection.Count,
+                            Is.Zero,
+                            $"Collection property '{displayName}' must initially be empty.");
+                    }
+                }
+            });
+        }
+
+        private static IEnumerable<TestCaseData> RepresentativeInterfacePropertyContracts()
+        {
+            yield return new TestCaseData(
+                typeof(IThing),
+                nameof(IThing.Id),
+                typeof(Guid));
+
+            yield return new TestCaseData(
+                typeof(IAuditableThing),
+                nameof(IAuditableThing.CreatedOn),
+                typeof(DateTime));
+
+            yield return new TestCaseData(
+                typeof(IBranchProtectionRule),
+                nameof(IBranchProtectionRule.MinimumRequiredApproval),
+                typeof(int));
+
+            yield return new TestCaseData(
+                typeof(IBranchProtectionRule),
+                nameof(IBranchProtectionRule.ReviewRequired),
+                typeof(bool));
+
+            yield return new TestCaseData(
+                typeof(IComment),
+                nameof(IComment.Content),
+                typeof(string));
+
+            yield return new TestCaseData(
+                typeof(IFunctionalProject),
+                nameof(IFunctionalProject.CurrentMode),
+                typeof(ProjectMode));
+
+            yield return new TestCaseData(
+                typeof(IFunctionalProject),
+                nameof(IFunctionalProject.BelongsTo),
+                typeof(Guid));
+
+            yield return new TestCaseData(
+                typeof(IProjectMember),
+                nameof(IProjectMember.ActiveOwnership),
+                typeof(Guid?));
+
+            yield return new TestCaseData(
+                typeof(IProjectMember),
+                nameof(IProjectMember.Owns),
+                typeof(List<Guid>));
+
+            yield return new TestCaseData(
+                typeof(IBranchProtectionRule),
+                nameof(IBranchProtectionRule.MergeAllowedFor),
+                typeof(List<ProjectMemberRole>));
+
+            yield return new TestCaseData(
+                typeof(IProjectMember),
+                nameof(IProjectMember.isOutsideCollaborator),
+                typeof(bool));
+        }
+
+        private static bool IsList(Type type)
+        {
+            return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>);
+        }
+
+        private static bool IsDerivedProperty(
+            Type declaringType,
+            PropertyInfo property)
+        {
+            return property.Name == nameof(IProjectMember.isOutsideCollaborator)
+                   && (declaringType == typeof(IProjectMember)
+                       || declaringType == typeof(ProjectMember));
+        }
+    }
+}
---
+++ Mycelium.SDK.Tests/Dto/DtoInheritanceTestFixture.cs
@@ -0,0 +1,149 @@
+// ------------------------------------------------------------------------------------------------
+//  <copyright file="DtoInheritanceTestFixture.cs" company="Starion Group S.A.">
+// 
+//    Copyright 2026 Starion Group S.A.
+//    SPDX-License-Identifier: Apache-2.0
+// 
+//  </copyright>
+//  ------------------------------------------------------------------------------------------------
+
+namespace Mycelium.SDK.Tests.Dto
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using Mycelium.SDK.DTO;
+
+    /// <summary>
+    /// Verifies inheritance and complete implementation of the generated
+    /// FunctionalData DTO contracts.
+    /// </summary>
+    [TestFixture]
+    public class DtoInheritanceTestFixture
+    {
+        private static readonly IReadOnlyDictionary<Type, Type[]> DirectGeneralizations =
+            new Dictionary<Type, Type[]>
+            {
+                [typeof(IThing)] = [],
+                [typeof(IAuditableThing)] = [typeof(IThing)],
+                [typeof(IBranchProtectionRule)] = [typeof(IAuditableThing)],
+                [typeof(IComment)] = [typeof(IAuditableThing)],
+                [typeof(IFunctionalProject)] = [typeof(IAuditableThing)],
+                [typeof(IFunctionalProjectPolicy)] = [typeof(IAuditableThing)],
+                [typeof(IOrganization)] = [typeof(IAuditableThing)],
+                [typeof(IOrganizationMember)] = [typeof(IAuditableThing)],
+                [typeof(IOrganizationPolicy)] = [typeof(IAuditableThing)],
+                [typeof(IOwnership)] = [typeof(IAuditableThing)],
+                [typeof(IProjectMember)] = [typeof(IAuditableThing)],
+                [typeof(IReview)] = [typeof(IAuditableThing)],
+                [typeof(IUser)] = [typeof(IAuditableThing)]
+            };
+
+        private static readonly IReadOnlyDictionary<Type, Type> ConcreteImplementations =
+            new Dictionary<Type, Type>
+            {
+                [typeof(IBranchProtectionRule)] = typeof(BranchProtectionRule),
+                [typeof(IComment)] = typeof(Comment),
+                [typeof(IFunctionalProject)] = typeof(FunctionalProject),
+                [typeof(IFunctionalProjectPolicy)] = typeof(FunctionalProjectPolicy),
+                [typeof(IOrganization)] = typeof(Organization),
+                [typeof(IOrganizationMember)] = typeof(OrganizationMember),
+                [typeof(IOrganizationPolicy)] = typeof(OrganizationPolicy),
+                [typeof(IOwnership)] = typeof(Ownership),
+                [typeof(IProjectMember)] = typeof(ProjectMember),
+                [typeof(IReview)] = typeof(Review),
+                [typeof(IUser)] = typeof(User)
+            };
+
+        [Test]
+        public void Verify_that_DTO_interface_inheritance_matches_UML_generalization()
+        {
+            Assert.Multiple(() =>
+            {
+                foreach (var expectedGeneralization in DirectGeneralizations)
+                {
+                    var actualGeneralizations = QueryDirectInterfaces(expectedGeneralization.Key);
+                    var expectedGeneralizations = expectedGeneralization.Value
+                        .OrderBy(type => type.Name, StringComparer.Ordinal)
+                        .ToArray();
+
+                    Assert.That(
+                        actualGeneralizations,
+                        Is.EqualTo(expectedGeneralizations),
+                        $"Interface '{expectedGeneralization.Key.Name}' has an unexpected direct base interface.");
+                }
+            });
+        }
+
+        [Test]
+        public void Verify_that_each_concrete_DTO_implements_its_complete_inherited_contract()
+        {
+            Assert.Multiple(() =>
+            {
+                foreach (var implementation in ConcreteImplementations)
+                {
+                    var interfaceType = implementation.Key;
+                    var concreteType = implementation.Value;
+
+                    Assert.That(
+                        interfaceType.IsAssignableFrom(concreteType),
+                        Is.True,
+                        $"'{concreteType.Name}' does not implement '{interfaceType.Name}'.");
+
+                    var expectedProperties = QueryCompleteInterfaceProperties(interfaceType)
+                        .Select(QueryPropertySignature)
+                        .OrderBy(signature => signature, StringComparer.Ordinal)
+                        .ToArray();
+
+                    var actualProperties = concreteType
+                        .GetProperties(
+                            BindingFlags.Public
+                            | BindingFlags.Instance
+                            | BindingFlags.DeclaredOnly)
+                        .Select(QueryPropertySignature)
+                        .OrderBy(signature => signature, StringComparer.Ordinal)
+                        .ToArray();
+
+                    Assert.That(
+                        actualProperties,
+                        Is.EqualTo(expectedProperties),
+                        $"'{concreteType.Name}' does not implement the complete inherited interface contract.");
+                }
+            });
+        }
+
+        private static IEnumerable<PropertyInfo> QueryCompleteInterfaceProperties(
+            Type interfaceType)
+        {
+            return interfaceType
+                .GetInterfaces()
+                .Append(interfaceType)
+                .SelectMany(type =>
+                    type.GetProperties(
+                        BindingFlags.Public
+                        | BindingFlags.Instance
+                        | BindingFlags.DeclaredOnly));
+        }
+
+        private static Type[] QueryDirectInterfaces(Type interfaceType)
+        {
+            var allInterfaces = interfaceType.GetInterfaces();
+
+            var indirectInterfaces = allInterfaces
+                .SelectMany(candidate => candidate.GetInterfaces())
+                .ToHashSet();
+
+            return allInterfaces
+                .Where(candidate => !indirectInterfaces.Contains(candidate))
+                .OrderBy(candidate => candidate.Name, StringComparer.Ordinal)
+                .ToArray();
+        }
+
+        private static string QueryPropertySignature(PropertyInfo property)
+        {
+            return $"{property.Name}:{property.PropertyType}";
+        }
+    }
+}
