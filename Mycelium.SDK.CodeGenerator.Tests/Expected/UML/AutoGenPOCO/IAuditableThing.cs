ce15c7578a2d73ef6bbd51986de3f1681ecf21de
TanaseMariusatstarion <tm.manafu@stariongroup.eu>
2026-08-07 10:33:10 +0200

    a couple of more golden tests for POCO

---
+++ Mycelium.SDK.CodeGenerator.Tests/Expected/UML/AutoGenPOCO/IAuditableThing.cs
@@ -0,0 +1,51 @@
+// ------------------------------------------------------------------------------------------------
+//  <copyright file="IAuditableThing.cs" company="Starion Group S.A.">
+//
+//    Copyright 2026 Starion Group S.A.
+//    SPDX-License-Identifier: Apache-2.0
+//
+//  </copyright>
+//  ------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------
+// --------THIS IS AN AUTOMATICALLY GENERATED FILE. ANY MANUAL CHANGES WILL BE OVERWRITTEN!--------
+// ------------------------------------------------------------------------------------------------
+
+namespace Mycelium.SDK.POCO
+{
+    using System;
+    using System.CodeDom.Compiler;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Any <see cref="Thing" /> that shall record creation and update date and authority.
+    /// </summary>
+    [GeneratedCode("Mycelium.SDK", "latest")]
+    public partial interface IAuditableThing : IThing
+    {
+        /// <summary>
+        /// References the <see cref="User" /> that created the current <see cref="AuditableThing" />.
+        /// </summary>
+        IUser CreatedBy { get; set; }
+
+        /// <summary>
+        /// Provides the creation <see cref="DateTime" /> of the current <see cref="AuditableThing" />
+        /// </summary>
+        DateTime CreatedOn { get; set; }
+
+        /// <summary>
+        /// References the <see cref="User" /> that provide the last update on the current
+        /// <see cref="AuditableThing" />.
+        /// </summary>
+        IUser UpdatedBy { get; set; }
+
+        /// <summary>
+        /// Provides the last modification <see cref="DateTime" /> of the current <see cref="AuditableThing" />
+        /// </summary>
+        DateTime UpdatedOn { get; set; }
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
+// --------THIS IS AN AUTOMATICALLY GENERATED FILE. ANY MANUAL CHANGES WILL BE OVERWRITTEN!--------
+// ------------------------------------------------------------------------------------------------
