40309587e4d02409bb7e79ffe5d554ec2ffa3f31
TanaseMariusatstarion <tm.manafu@stariongroup.eu>
2026-08-17 09:33:50 +0200

    final fixes for duplication of more than 3 percent

--- Mycelium.SDK.CodeGenerator/Generators/UmlHandleBarsGenerators/UmlDtoGenerator.cs
+++ Mycelium.SDK.CodeGenerator/Generators/UmlHandleBarsGenerators/UmlDtoGenerator.cs
@@ -10,20 +10,17 @@
 namespace Mycelium.SDK.CodeGenerator.Generators.UmlHandleBarsGenerators
 {
     using System;
-    using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
     using System.Threading.Tasks;
 
     using Mycelium.SDK.CodeGenerator.HandleBarHelpers;
 
     using uml4net.StructuredClassifiers;
-    using uml4net.xmi.Readers;
 
     /// <summary>
     /// Generates the FunctionalData DTO interfaces and concrete implementations.
     /// </summary>
-    public sealed class UmlDtoGenerator : UmlHandleBarsGenerator
+    public sealed class UmlDtoGenerator : UmlClassHandleBarsGenerator
     {
         /// <summary>
         /// The registered Handlebars template name used for concrete DTO classes.
@@ -36,33 +33,13 @@ namespace Mycelium.SDK.CodeGenerator.Generators.UmlHandleBarsGenerators
         private const string InterfaceTemplateName = "dto-interface-uml-template";
 
         /// <inheritdoc />
-        public override async Task GenerateAsync(XmiReaderResult xmiReaderResult, DirectoryInfo outputDirectory)
-        {
-            ArgumentNullException.ThrowIfNull(xmiReaderResult);
-            ArgumentNullException.ThrowIfNull(outputDirectory);
+        protected override string ArtifactName => "DTO";
 
-            var payload = CreateHandlebarsPayload(xmiReaderResult);
+        /// <inheritdoc />
+        protected override string ClassTemplate => ClassTemplateName;
 
-            // Render and validate the complete batch before creating or writing
-            // the output directory. This prevents partial output when any model
-            // element cannot be generated.
-            var generatedFiles = payload.Classes
-                .Select(this.RenderDataTransferObjectInterface)
-                .Concat(payload.Classes
-                        .Where(umlClass => !umlClass.IsAbstract)
-                        .Select(this.RenderDataTransferObjectClass))
-                .OrderBy(generatedFile => generatedFile.FileName, StringComparer.Ordinal)
-                .ToArray();
-
-            ThrowIfDuplicateFileNames(generatedFiles);
-
-            outputDirectory.Create();
-
-            foreach (var generatedFile in generatedFiles)
-            {
-                await WriteAsync(generatedFile.Source, outputDirectory, generatedFile.FileName);
-            }
-        }
+        /// <inheritdoc />
+        protected override string InterfaceTemplate => InterfaceTemplateName;
 
         /// <summary>
         /// Generates one DTO interface.
@@ -88,18 +65,11 @@ namespace Mycelium.SDK.CodeGenerator.Generators.UmlHandleBarsGenerators
         /// <exception cref="ArgumentException">
         /// Thrown when a modeled name cannot be represented as a legal C# identifier.
         /// </exception>
-        public async Task<string> GenerateDataTransferObjectInterfaceAsync(DirectoryInfo outputDirectory, IClass umlClass)
+        public Task<string> GenerateDataTransferObjectInterfaceAsync(
+            DirectoryInfo outputDirectory,
+            IClass umlClass)
         {
-            ArgumentNullException.ThrowIfNull(outputDirectory);
-            ArgumentNullException.ThrowIfNull(umlClass);
-
-            var generatedFile = this.RenderDataTransferObjectInterface(umlClass);
-
-            outputDirectory.Create();
-
-            await WriteAsync(generatedFile.Source, outputDirectory, generatedFile.FileName);
-
-            return generatedFile.Source;
+            return this.GenerateInterfaceAsync(outputDirectory, umlClass);
         }
 
         /// <summary>
@@ -126,141 +96,16 @@ namespace Mycelium.SDK.CodeGenerator.Generators.UmlHandleBarsGenerators
         /// <exception cref="ArgumentException">
         /// Thrown when a modeled name cannot be represented as a legal C# identifier.
         /// </exception>
-        public async Task<string> GenerateDataTransferObjectClassAsync(DirectoryInfo outputDirectory, IClass umlClass)
+        public Task<string> GenerateDataTransferObjectClassAsync(DirectoryInfo outputDirectory, IClass umlClass)
         {
-            ArgumentNullException.ThrowIfNull(outputDirectory);
-            ArgumentNullException.ThrowIfNull(umlClass);
-
-            var generatedFile = this.RenderDataTransferObjectClass(umlClass);
-
-            outputDirectory.Create();
-
-            await WriteAsync(generatedFile.Source, outputDirectory, generatedFile.FileName);
-
-            return generatedFile.Source;
+            return this.GenerateClassAsync(outputDirectory, umlClass);
         }
 
         /// <inheritdoc />
-        protected override void RegisterHelpers()
+        protected override void RegisterArtifactHelpers()
         {
-            DocumentationHelper.RegisterDocumentationHelper(this.Handlebars);
             ClassHelper.RegisterDtoClassHelper(this.Handlebars);
             PropertyHelper.RegisterDtoPropertyHelper(this.Handlebars);
-            NamedElementHelper.RegisterNamedElementHelper(this.Handlebars);
         }
-
-        /// <inheritdoc />
-        protected override void RegisterTemplates()
-        {
-            this.RegisterTemplate(ClassTemplateName);
-            this.RegisterTemplate(InterfaceTemplateName);
-        }
-
-        /// <summary>
-        /// Renders a DTO interface for the specified UML class.
-        /// </summary>
-        /// <param name="umlClass">
-        /// The UML class for which the DTO interface is rendered.
-        /// </param>
-        /// <returns>
-        /// The generated filename and formatted C# source.
-        /// </returns>
-        private GeneratedFile RenderDataTransferObjectInterface(IClass umlClass)
-        {
-            ArgumentNullException.ThrowIfNull(umlClass);
-
-            var className = QueryRequiredClassName(umlClass);
-            var generatedCode = this.Templates[InterfaceTemplateName](umlClass);
-
-            generatedCode = this.CodeCleanup(generatedCode);
-
-            return new GeneratedFile($"I{className}.cs", generatedCode);
-        }
-
-        /// <summary>
-        /// Renders a concrete DTO implementation for the specified UML class.
-        /// </summary>
-        /// <param name="umlClass">
-        /// The UML class for which the concrete DTO is rendered.
-        /// </param>
-        /// <returns>
-        /// The generated filename and formatted C# source.
-        /// </returns>
-        /// <exception cref="InvalidOperationException">
-        /// Thrown when the UML class is abstract or has no name.
-        /// </exception>
-        private GeneratedFile RenderDataTransferObjectClass(IClass umlClass)
-        {
-            ArgumentNullException.ThrowIfNull(umlClass);
-
-            if (umlClass.IsAbstract)
-            {
-                throw new InvalidOperationException(
-                    $"Cannot generate a concrete DTO implementation for abstract class "
-                    + $"'{QueryRequiredClassName(umlClass)}'.");
-            }
-
-            var className = QueryRequiredClassName(umlClass);
-            var generatedCode = this.Templates[ClassTemplateName](umlClass);
-
-            generatedCode = this.CodeCleanup(generatedCode);
-
-            return new GeneratedFile($"{className}.cs", generatedCode);
-        }
-
-        /// <summary>
-        /// Queries the required name of a UML class.
-        /// </summary>
-        /// <param name="umlClass">
-        /// The UML class whose name is queried.
-        /// </param>
-        /// <returns>
-        /// The UML class name.
-        /// </returns>
-        /// <exception cref="InvalidOperationException">
-        /// Thrown when the UML class has no name.
-        /// </exception>
-        private static string QueryRequiredClassName(IClass umlClass)
-        {
-            if (string.IsNullOrWhiteSpace(umlClass.Name))
-            {
-                throw new InvalidOperationException($"Class '{umlClass.XmiId}' has no name.");
-            }
-
-            return umlClass.Name;
-        }
-
-        /// <summary>
-        /// Verifies that a generated batch contains no duplicate filenames.
-        /// </summary>
-        /// <param name="generatedFiles">
-        /// The generated files to validate.
-        /// </param>
-        /// <exception cref="InvalidOperationException">
-        /// Thrown when multiple generated files have the same filename.
-        /// </exception>
-        private static void ThrowIfDuplicateFileNames(IReadOnlyCollection<GeneratedFile> generatedFiles)
-        {
-            var duplicateFileName = generatedFiles
-                .GroupBy(generatedFile => generatedFile.FileName, StringComparer.Ordinal)
-                .FirstOrDefault(group => group.Count() > 1)
-                ?.Key;
-
-            if (duplicateFileName is not null)
-            {
-                throw new InvalidOperationException($"DTO generation produced duplicate filename " + $"'{duplicateFileName}'.");
-            }
-        }
-
-        /// <summary>
-        /// Represents one generated source file.
-        /// </summary>
-        /// <param name="FileName">
-        /// The generated filename.
-        /// </param>
-        /// <param name="Source">
-        /// The formatted generated C# source.
-        /// </param>
-        private sealed record GeneratedFile(string FileName, string Source);
     }
 }
--- Mycelium.SDK.CodeGenerator/Generators/UmlHandleBarsGenerators/UmlPocoGenerator.cs
+++ Mycelium.SDK.CodeGenerator/Generators/UmlHandleBarsGenerators/UmlPocoGenerator.cs
@@ -10,20 +10,17 @@
 namespace Mycelium.SDK.CodeGenerator.Generators.UmlHandleBarsGenerators
 {
     using System;
-    using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
     using System.Threading.Tasks;
 
     using Mycelium.SDK.CodeGenerator.HandleBarHelpers;
 
     using uml4net.StructuredClassifiers;
-    using uml4net.xmi.Readers;
 
     /// <summary>
     /// Generates the FunctionalData POCO interfaces and concrete implementations.
     /// </summary>
-    public sealed class UmlPocoGenerator : UmlHandleBarsGenerator
+    public sealed class UmlPocoGenerator : UmlClassHandleBarsGenerator
     {
         /// <summary>
         /// The registered Handlebars template name used for concrete POCO classes.
@@ -36,33 +33,13 @@ namespace Mycelium.SDK.CodeGenerator.Generators.UmlHandleBarsGenerators
         private const string InterfaceTemplateName = "poco-interface-uml-template";
 
         /// <inheritdoc />
-        public override async Task GenerateAsync(XmiReaderResult xmiReaderResult, DirectoryInfo outputDirectory)
-        {
-            ArgumentNullException.ThrowIfNull(xmiReaderResult);
-            ArgumentNullException.ThrowIfNull(outputDirectory);
+        protected override string ArtifactName => "POCO";
 
-            var payload = CreateHandlebarsPayload(xmiReaderResult);
+        /// <inheritdoc />
+        protected override string ClassTemplate => ClassTemplateName;
 
-            // Render and validate the complete batch before creating or writing
-            // the output directory. This prevents partial output when any model
-            // element cannot be generated.
-            var generatedFiles = payload.Classes
-                .Select(this.RenderPocoInterface)
-                .Concat(payload.Classes
-                        .Where(umlClass => !umlClass.IsAbstract)
-                        .Select(this.RenderPocoClass))
-                .OrderBy(generatedFile => generatedFile.FileName, StringComparer.Ordinal)
-                .ToArray();
-
-            ThrowIfDuplicateFileNames(generatedFiles);
-
-            outputDirectory.Create();
-
-            foreach (var generatedFile in generatedFiles)
-            {
-                await WriteAsync(generatedFile.Source, outputDirectory, generatedFile.FileName);
-            }
-        }
+        /// <inheritdoc />
+        protected override string InterfaceTemplate => InterfaceTemplateName;
 
         /// <summary>
         /// Generates one POCO interface.
@@ -88,18 +65,9 @@ namespace Mycelium.SDK.CodeGenerator.Generators.UmlHandleBarsGenerators
         /// <exception cref="ArgumentException">
         /// Thrown when a modeled name cannot be represented as a legal C# identifier.
         /// </exception>
-        public async Task<string> GeneratePocoInterfaceAsync(DirectoryInfo outputDirectory, IClass umlClass)
+        public Task<string> GeneratePocoInterfaceAsync(DirectoryInfo outputDirectory, IClass umlClass)
         {
-            ArgumentNullException.ThrowIfNull(outputDirectory);
-            ArgumentNullException.ThrowIfNull(umlClass);
-
-            var generatedFile = this.RenderPocoInterface(umlClass);
-
-            outputDirectory.Create();
-
-            await WriteAsync(generatedFile.Source, outputDirectory, generatedFile.FileName);
-
-            return generatedFile.Source;
+            return this.GenerateInterfaceAsync(outputDirectory, umlClass);
         }
 
         /// <summary>
@@ -126,141 +94,16 @@ namespace Mycelium.SDK.CodeGenerator.Generators.UmlHandleBarsGenerators
         /// <exception cref="ArgumentException">
         /// Thrown when a modeled name cannot be represented as a legal C# identifier.
         /// </exception>
-        public async Task<string> GeneratePocoClassAsync(DirectoryInfo outputDirectory, IClass umlClass)
+        public Task<string> GeneratePocoClassAsync(DirectoryInfo outputDirectory, IClass umlClass)
         {
-            ArgumentNullException.ThrowIfNull(outputDirectory);
-            ArgumentNullException.ThrowIfNull(umlClass);
-
-            var generatedFile = this.RenderPocoClass(umlClass);
-
-            outputDirectory.Create();
-
-            await WriteAsync(generatedFile.Source, outputDirectory, generatedFile.FileName);
-
-            return generatedFile.Source;
+            return this.GenerateClassAsync(outputDirectory, umlClass);
         }
 
         /// <inheritdoc />
-        protected override void RegisterHelpers()
+        protected override void RegisterArtifactHelpers()
         {
-            DocumentationHelper.RegisterDocumentationHelper(this.Handlebars);
             ClassHelper.RegisterPocoClassHelper(this.Handlebars);
             PropertyHelper.RegisterPocoPropertyHelper(this.Handlebars);
-            NamedElementHelper.RegisterNamedElementHelper(this.Handlebars);
         }
-
-        /// <inheritdoc />
-        protected override void RegisterTemplates()
-        {
-            this.RegisterTemplate(ClassTemplateName);
-            this.RegisterTemplate(InterfaceTemplateName);
-        }
-
-        /// <summary>
-        /// Renders a POCO interface for the specified UML class.
-        /// </summary>
-        /// <param name="umlClass">
-        /// The UML class for which the POCO interface is rendered.
-        /// </param>
-        /// <returns>
-        /// The generated filename and formatted C# source.
-        /// </returns>
-        private GeneratedFile RenderPocoInterface(IClass umlClass)
-        {
-            ArgumentNullException.ThrowIfNull(umlClass);
-
-            var className = QueryRequiredClassName(umlClass);
-            var generatedCode = this.Templates[InterfaceTemplateName](umlClass);
-
-            generatedCode = this.CodeCleanup(generatedCode);
-
-            return new GeneratedFile($"I{className}.cs", generatedCode);
-        }
-
-        /// <summary>
-        /// Renders a concrete POCO implementation for the specified UML class.
-        /// </summary>
-        /// <param name="umlClass">
-        /// The UML class for which the concrete POCO is rendered.
-        /// </param>
-        /// <returns>
-        /// The generated filename and formatted C# source.
-        /// </returns>
-        /// <exception cref="InvalidOperationException">
-        /// Thrown when the UML class is abstract or has no name.
-        /// </exception>
-        private GeneratedFile RenderPocoClass(IClass umlClass)
-        {
-            ArgumentNullException.ThrowIfNull(umlClass);
-
-            if (umlClass.IsAbstract)
-            {
-                throw new InvalidOperationException(
-                    $"Cannot generate a concrete POCO implementation for abstract class "
-                    + $"'{QueryRequiredClassName(umlClass)}'.");
-            }
-
-            var className = QueryRequiredClassName(umlClass);
-            var generatedCode = this.Templates[ClassTemplateName](umlClass);
-
-            generatedCode = this.CodeCleanup(generatedCode);
-
-            return new GeneratedFile($"{className}.cs", generatedCode);
-        }
-
-        /// <summary>
-        /// Queries the required name of a UML class.
-        /// </summary>
-        /// <param name="umlClass">
-        /// The UML class whose name is queried.
-        /// </param>
-        /// <returns>
-        /// The UML class name.
-        /// </returns>
-        /// <exception cref="InvalidOperationException">
-        /// Thrown when the UML class has no name.
-        /// </exception>
-        private static string QueryRequiredClassName(IClass umlClass)
-        {
-            if (string.IsNullOrWhiteSpace(umlClass.Name))
-            {
-                throw new InvalidOperationException($"Class '{umlClass.XmiId}' has no name.");
-            }
-
-            return umlClass.Name;
-        }
-
-        /// <summary>
-        /// Verifies that a generated batch contains no duplicate filenames.
-        /// </summary>
-        /// <param name="generatedFiles">
-        /// The generated files to validate.
-        /// </param>
-        /// <exception cref="InvalidOperationException">
-        /// Thrown when multiple generated files have the same filename.
-        /// </exception>
-        private static void ThrowIfDuplicateFileNames(IReadOnlyCollection<GeneratedFile> generatedFiles)
-        {
-            var duplicateFileName = generatedFiles
-                .GroupBy(generatedFile => generatedFile.FileName, StringComparer.Ordinal)
-                .FirstOrDefault(group => group.Count() > 1)
-                ?.Key;
-
-            if (duplicateFileName is not null)
-            {
-                throw new InvalidOperationException($"POCO generation produced duplicate filename " + $"'{duplicateFileName}'.");
-            }
-        }
-
-        /// <summary>
-        /// Represents one generated source file.
-        /// </summary>
-        /// <param name="FileName">
-        /// The generated filename.
-        /// </param>
-        /// <param name="Source">
-        /// The formatted generated C# source.
-        /// </param>
-        private sealed record GeneratedFile(string FileName, string Source);
     }
 }
