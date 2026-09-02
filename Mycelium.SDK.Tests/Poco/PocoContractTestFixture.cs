c5f72fa1eae6d3aa6a8f84c1321dd3fda2638baa
Tanase Marius <97907096+MariusMTM@users.noreply.github.com>
2026-09-02 19:16:12 +0300

    `fix POCO contract discovery for generated types`

--- Mycelium.SDK.Tests/Poco/PocoContractTestFixture.cs
+++ Mycelium.SDK.Tests/Poco/PocoContractTestFixture.cs
@@ -10,6 +10,7 @@
 namespace Mycelium.SDK.Tests.Poco
 {
     using System;
+    using System.CodeDom.Compiler;
     using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
@@ -377,7 +378,8 @@ namespace Mycelium.SDK.Tests.Poco
                 .Where(
                     type => type.Namespace == PocoNamespace
                             && type.IsClass
-                            && !type.IsAbstract)
+                            && !type.IsAbstract
+                            && type.GetCustomAttribute<GeneratedCodeAttribute>() is not null)
                 .OrderBy(type => type.Name, StringComparer.Ordinal)
                 .ToArray();
         }
